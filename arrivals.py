d5169bbc3890715066a3a126bb6bd51a6ebdea8f
Urbanek Matthew <mu529@compute.cusp.nyu.edu>
2016-06-13 15:54:52 -0400

    added code comments to arrivals module

--- arrivals.py
+++ arrivals.py
@@ -2,20 +2,29 @@
 """
 Created on Wed Jun  8 18:18:01 2016
 
+This module is meant for methods used for estimating actual vehicle arrival
+times, whether it be at a certain stop or intersection etc. There may be
+multiple techniques to make a certain estimation.
+
 @author: mu529
 """
 
 from scipy import spatial
 
 def nearby_pings(stop_id,trip_id,stop_times,stops,avl_data,radius=0.001):
-    # return the AVL pings near a certain stop corresponding to a certain trip
-    trip_stop_times = stop_times.loc[trip_id]
+    # returns the AVL pings near a certain stop corresponding to a certain trip
+    trip_stop_times = stop_times.loc[trip_id] # stop_times only for this trip
+    # get only the interesting columns from stop reference data ...
     stop_locs = stops[['stop_lat','stop_lon','stop_name']]
-    trip_stop_times = trip_stop_times.join(stop_locs)
-    lookup = 'MTA NYCT_' + trip_id
+    trip_stop_times = trip_stop_times.join(stop_locs) #... and join to times
+    lookup = 'MTA NYCT_' + trip_id # lookup string used to query AVL data
     avl_subset = avl_data.query('Trip == @lookup')
+    # create array of only the lon-lat values from AVL data
     points = zip(avl_subset.Longitude.ravel(), avl_subset.Latitude.ravel())
-    tree = spatial.KDTree(points)
+    tree = spatial.KDTree(points) # implement KDTree class
+    # get the lon and lat of the user-specified stop_id
     lookup_point = [trip_stop_times.loc[stop_id].stop_lon,trip_stop_times.loc[stop_id].stop_lat]
+    # and get index of AVL records that are nearby to the desired point
     nearby_points = tree.query_ball_point(lookup_point, radius)
+    # return the AVL records
     return avl_subset.iloc[nearby_points]
