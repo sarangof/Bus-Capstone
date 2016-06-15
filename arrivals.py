36cde70eeb7b417b9fb8103bfb084835c2b7ec79
Sara Arango <sarangof@nyu.edu>
2016-06-15 11:12:38 -0400

    Only finding points around a given (not necessarily stop) location.

--- arrivals.py
+++ arrivals.py
@@ -12,21 +12,32 @@ multiple techniques to make a certain estimation.
 from scipy import spatial
 
 def nearby_pings(stop_id,trip_id,stop_times,stops,avl_data,radius=0.001):
-    # returns the AVL pings near a certain stop corresponding to a certain trip
-    trip_stop_times = stop_times.loc[trip_id] # stop_times only for this trip
-    # get only the interesting columns from stop reference data ...
-    stop_locs = stops[['stop_lat','stop_lon','stop_name']]
-    trip_stop_times = trip_stop_times.join(stop_locs) #... and join to times
-    lookup = 'MTA NYCT_' + trip_id # lookup string used to query AVL data
-    avl_subset = avl_data.query('Trip == @lookup')
-    # create array of only the lon-lat values from AVL data
-    points = zip(avl_subset.Longitude.ravel(), avl_subset.Latitude.ravel())
-    tree = spatial.KDTree(points) # implement KDTree class
-    # get the lon and lat of the user-specified stop_id
-    lookup_point = [trip_stop_times.loc[stop_id].stop_lon,trip_stop_times.loc[stop_id].stop_lat]
-    # and get index of AVL records that are nearby to the desired point
-    nearby_points = tree.query_ball_point(lookup_point, radius)
-    #nearest_point = tree.query(lookup_point,1)
-    #print nearest_point
-    # return the AVL records
-    return avl_subset.iloc[nearby_points]
+	# returns the AVL pings near a certain stop corresponding to a certain trip
+	trip_stop_times = stop_times.loc[trip_id] # stop_times only for this trip
+	# get only the interesting columns from stop reference data ...
+	stop_locs = stops[['stop_lat','stop_lon','stop_name']]
+	trip_stop_times = trip_stop_times.join(stop_locs) #... and join to times
+	lookup = 'MTA NYCT_' + trip_id # lookup string used to query AVL data
+	avl_subset = avl_data.query('Trip == @lookup')
+	# create array of only the lon-lat values from AVL data
+	points = zip(avl_subset.Longitude.ravel(), avl_subset.Latitude.ravel())
+	tree = spatial.KDTree(points) # implement KDTree class
+	# get the lon and lat of the user-specified stop_id
+	lookup_point = [trip_stop_times.loc[stop_id].stop_lon,trip_stop_times.loc[stop_id].stop_lat]
+	# and get index of AVL records that are nearby to the desired point
+	nearby_points = tree.query_ball_point(lookup_point, radius)
+	#nearest_point = tree.query(lookup_point,1)
+	#print nearest_point
+	# return the AVL records
+	return avl_subset.iloc[nearby_points]
+
+def time_at_location(lat,lon,trip_id,avl_data,stop_times,stops):
+	trip_stop_times = stop_times.loc[trip_id] 
+	lookup = 'MTA NYCT_' + trip_id # lookup string used to query AVL data
+	avl_subset = avl_data.query('Trip == @lookup')
+	points = zip(avl_subset.Longitude.ravel(), avl_subset.Latitude.ravel())
+	tree = spatial.KDTree(points) # implement KDTree class
+	lookup_point = [lon,lat]
+	nearby_points = tree.query_ball_point(lookup_point, radius)
+	return avl_subset.iloc[nearby_points]
+	
