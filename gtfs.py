207ca24b3d8144b396bb40257beaf12ec95cc267
Matthew Urbanek <mu529@nyu.edu>
2016-07-03 08:28:31 -0400

    first method added to class TransitCalendar

--- gtfs.py
+++ gtfs.py
@@ -96,23 +96,24 @@ def load_trip_shapes(dpath,clean=True):
         pass
     trip_shapes.set_index('shape_id',drop=True,inplace=True,verify_integrity=True)
     return trip_shapes
-
+    
 class TransitCalendar:
-    dpath = '/gpfs2/projects/project-bus_capstone_2016/workspace/share/gtfs/'
-    service_dates = pd.DataFrame()
-    for fname in os.listdir(dpath):    
-        try:
-            with ZipFile(dpath+fname) as zf:
-                raw_text = zf.read('calendar.txt')    
-            csvdata=StringIO(raw_text)
-            service_dates = service_dates.append(pd.read_csv(csvdata))
-        except:
-            print 'Error reading from ' + fname
-    service_dates.set_index('service_id',drop=True,inplace=True,verify_integrity=True)
-    import calendar
-    trip_date = '2016-06-13'
-    trip_dow = calendar.weekday(int(trip_date[:4]),int(trip_date[5:7]),int(trip_date[8:10]))
-    trip_date = int(trip_date.replace('-',''))
-    bools = (service_dates.start_date <= trip_date) & (trip_date <= service_dates.end_date)
-    bools = service_dates.ix[bools,trip_dow] == 1
-    list(service_dates.loc[bools].index)
+    def __init__(self, dpath):
+        service_dates = pd.DataFrame()
+        for fname in os.listdir(dpath):    
+            try:
+                with ZipFile(dpath+fname) as zf:
+                    raw_text = zf.read('calendar.txt')    
+                csvdata=StringIO(raw_text)
+                service_dates = service_dates.append(pd.read_csv(csvdata))
+            except:
+                print 'Error reading from ' + fname
+        service_dates.set_index('service_id',drop=True,inplace=True,verify_integrity=True)
+        self.service_dates = service_dates
+    def get_service_ids(self,d):
+        import calendar
+        trip_dow = calendar.weekday(int(d[:4]),int(d[5:7]),int(d[8:10]))
+        trip_date = int(d.replace('-',''))    
+        bools = (self.service_dates.start_date <= trip_date) & (trip_date <= self.service_dates.end_date)       
+        subset = self.service_dates.loc[bools]
+        return list(subset[subset.iloc[:,trip_dow]==1].index)
